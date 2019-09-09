658cfa5751691abc982e09ec61db8cc996049cd0
Doug Woodrow <doug@joinknack.com>
2019-09-09 10:08:18 -0400

    Added -n flag to specify a number of days back to get and -w for grabbing the commits from the past week after the weekend

--- git-logger.sh
+++ git-logger.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-after=`date -v-1d "+%F"`
+number_days_back=1
+
+while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
+  -n | --number )
+	shift;
+    number_days_back=$1
+    ;;
+  -w | --weekend )
+    number_days_back=3
+    ;;
+esac; shift; done
+if [[ "$1" == '--' ]]; then shift; fi
+
+number_days_back=${number_days_back}d
+after=`date -v-$number_days_back "+%F"`
 after="$after 00:00"
-before=`date -v-1d +%F`
+before=`date -v-$number_days_back "+%F"`
 before="$before 23:59"
 
 logs=`git --no-pager log --before="$before" --after="$after" --author="Doug Woodrow" --format=%B` 
--- install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo chmod +x ./get-past-day.sh
-cp ./get-past-day.sh /usr/local/bin/git-logger
